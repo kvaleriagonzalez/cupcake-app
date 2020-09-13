d5393274298227ffb20e7d2c76d0d9a5191ae2a1
karen <k.valeria.gonzalez@gmail.com>
2020-09-13 01:40:56 -0400

    jquery and styles changes

--- main.js
+++ main.js
@@ -2,21 +2,26 @@
 // get document ready//
 $(document).ready(function () {
       // Create an event listener for a 'change' event//
-      $('input:checkbox').change(function () {
-            //variable to match picture location-to display image when the option is clicked
-            const display = $(this).prop('name');
-            if ($(this).is(':checked'))
-             // Display the image on the page!//
-                  $('div.options.photo').append(`<img src=./assets/${display}.png/>`)
-            //remove image if another option is selected
-            else {
-                   $(`img[src=./assets/${display}.png]`.remove());
+      $('input:radio').change(function () {
+            // write variable for value (image)
+            const display = $(this).prop('value');
+            // write variable for name
+            const name = $(this).prop('name')
+            // Display the image on the page!
+            $('#' + name).attr('src', display)
 
-           }  
       })
+    // when window reach 600px alert the user to turn cellphone to landscape//
+    // create an event listener for a resize event
+      $(window).resize(function () {
+        // if user reach 600px alert 
+        if ($(window).width() <= 600) {
+          alert("turn your cellphone to landscape");
+        } else {
+          console.log('created at Juno College')
+        }
+      });
+
+
 });
-
-
-
-
 
